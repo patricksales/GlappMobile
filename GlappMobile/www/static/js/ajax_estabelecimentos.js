0efea972b79a451e4dd2ae46f88b35d30f34db63
Gustavo Almansa Rodrigues <gustavo.almansa@gmail.com>
2015-10-25 04:10:28 -0200

    Merge origin/master
    Conflicts:
    	GlappMobile/platforms/android/assets/www/index.html
    	GlappMobile/platforms/android/assets/www/listaEstabelecimento.html
    	GlappMobile/platforms/android/assets/www/listaRestaurante.html
    	GlappMobile/platforms/android/assets/www/static/js/ajax_estabelecimentos.js
    	GlappMobile/www/index.html
    	GlappMobile/www/static/js/ajax_estabelecimentos.js

--- GlappMobile/www/static/js/ajax_estabelecimentos.js
+++ GlappMobile/www/static/js/ajax_estabelecimentos.js
@@ -3,27 +3,12 @@ var DialogModalMapa;
 var centroMapa = {lat: -23.5505200, lng: -46.6333090};
 var map, geocoder, directionsDisplay, directionsService, marker, infoWindow, infoWindowEvent;
 var listaObj = $("#lista-estabelecimento");
+var btnPesquisar = $("#btn-pesquisar");
 
-var inserirMarcador = function (posicao, titulo, informacao) {
-    var icon = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
-    var mapAux = map;
-    if (marker === undefined) {
-        marker = new google.maps.Marker({
-            title: titulo,
-            map: mapAux,
-            position: posicao,
-            icon: icon
-        });
-
-    } else {
-        marker.setTitle(titulo);
-        marker.setPosition(posicao);
-        marker.setIcon(icon);
-        marker.setMap(mapAux);
-    }
-
-    anexarMensagem(marker, informacao);
-
+var ativarCollapsible = function () {
+    $('.collapsible').collapsible({
+        accordion: true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
 };
 
 var anexarMensagem = function (marker, conteudo) {
@@ -39,9 +24,28 @@ var anexarMensagem = function (marker, conteudo) {
 
 };
 
+var inserirMarcador = function (posicao, titulo, informacao) {
+    var icon = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
+    var mapAux = map;
+    if (marker === undefined) {
+        marker = new google.maps.Marker({
+            title: titulo,
+            map: mapAux,
+            position: posicao,
+            icon: icon
+        });
+    } else {
+        marker.setTitle(titulo);
+        marker.setPosition(posicao);
+        marker.setIcon(icon);
+        marker.setMap(mapAux);
+    }
+
+    anexarMensagem(marker, informacao);
+};
+
 var recarregarMapa = function (id, latLng, objDescricao) {
     $('#map').appendTo('#mapContainer-' + id);
-    
     var mapParentWidth = $('#mapContainer-' + id).width();
     $('#map').width(mapParentWidth * 0.999);
     $('#map').height(mapParentWidth * 0.999);
@@ -50,30 +54,7 @@ var recarregarMapa = function (id, latLng, objDescricao) {
     inserirMarcador(latLng, tituloMarcador, informacaoBalao);
     google.maps.event.trigger(map, "resize");
     map.setCenter(latLng);
-
 };
-
-
-var initialize = function () {
-
-    directionsService = new google.maps.DirectionsService();
-    geocoder = new google.maps.Geocoder();
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 16,
-        center: centroMapa
-    });
-    var rendererOptions = {
-        map: map,
-        suppressMarkers: true
-    };
-
-    var anexarMensagem = function (marker, conteudo) {
-        //console.log("Chamado");
-        var infowindow = new google.maps.InfoWindow({maxWidth: 400, content: conteudo});
-        google.maps.event.addListener(marker, 'click', function () {
-            infowindow.open(marker.get('map'), marker);
-        });
-    };
 
 var renderizarLista = function (dadosRecebidos) {
     dadosRecebidos.forEach(function (estabelecimento) {
@@ -89,7 +70,6 @@ var renderizarLista = function (dadosRecebidos) {
         conteudo += '<input type="hidden" name="nome" value="' + estabelecimento.nome + '"  />';
         conteudo += '<input type="hidden" name="unidade" value="' + estabelecimento.unidade + '"  />';
         conteudo += '<div id="mapContainer-' + estabelecimento.idEmpresa + '">';
-        //conteudo += '<div id="map-' + estabelecimento.idEmpresa + '"></div>';
         conteudo += '</div>';
         div2.html(conteudo);
         li.append(div1);
@@ -98,125 +78,81 @@ var renderizarLista = function (dadosRecebidos) {
     });
 };
 
-        var mapParentWidth = $('#mapContainer-' + id).width();
-        $('#map-' + id).width(mapParentWidth * 0.999);
-        $('#map-' + id).height(mapParentWidth * 0.999);
-        var textoInformação = "<div><p>Teste</p></div>";
-        inserirMarcador(latLng, textoInformação);
-        google.maps.event.trigger(map, "resize");
-        setTimeout(function () {
-            map.setCenter(latLng);
-        }, 500);
-    };
-
-
-    var initialize = function (id, latLng) {
-
-        directionsService = new google.maps.DirectionsService();
-        geocoder = new google.maps.Geocoder();
-        map = new google.maps.Map(document.getElementById('map-' + id), {
-            zoom: 16,
-            center: latLng
-        });
-        var rendererOptions = {
-            map: map,
-            suppressMarkers: true
-        };
-        directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
-    };
-
-    $(".btn-mapa").on("click", function () {
-        var id = $(this).parent("div").find("input[name='id']").val();
-        var divPai = $(this).parent("div");
-        var lat = parseFloat(divPai.find("input[name='latitude']").val());
-        var lng = parseFloat(divPai.find("input[name='longitude']").val());
-        var nome = divPai.find("input[name='nome']").val();
-        var unidade = divPai.find("input[name='unidade']").val();
-        var objDescricao = {nome: nome, unidade: unidade};
-        var objLatLng = {lat: lat, lng: lng};
-        recarregarMapa(id, objLatLng, objDescricao);
-    });
+var obterInformacoesEstabelecimento = function (e) {
+    var divPai = $(e.target).parent("div");
+    var id = divPai.find("input[name='id']").val();
+    var lat = parseFloat(divPai.find("input[name='latitude']").val());
+    var lng = parseFloat(divPai.find("input[name='longitude']").val());
+    var nome = divPai.find("input[name='nome']").val();
+    var unidade = divPai.find("input[name='unidade']").val();
+    var objDescricao = {nome: nome, unidade: unidade};
+    var objLatLng = {lat: lat, lng: lng};
+    recarregarMapa(id, objLatLng, objDescricao);
 };
-
-var inicializarPagina = function () {
-    buscarEstabelecimentos();
-    initialize();
-};
-
-    /*var buscarEstabelecimentos = function () {
-     $.getJSON(URL + "/estabelecimento/all")
-     .success(renderizarLista)
-     .then(ativarCollapsible)
-     .then(vincularEventos)
-     ;
-     };*/
-
-    var vincularEventos = function () {
-
-        $(".btn-mapa").on("click", function () {
-            var id = $(this).parent("div").find("input[name='id']").val();
-            var lat = parseFloat($(this).parent("div").find("input[name='latitude']").val());
-            var lng = parseFloat($(this).parent("div").find("input[name='longitude']").val());
-            var objLatLng = {lat: lat, lng: lng};
-            initialize(id, objLatLng);
-            recarregarMapa(id, objLatLng);
-        });
-    };
-
-    var inicializarPagina = function () {
-        //buscarEstabelecimentos();
-        vincularEventos();
-        vincularEventosClick();
-    };
-
 
 
 //PARTE DE CONSULTA ESTABELECIMENTO
+var buscarNomeEstabelecimento = function (termoPesquisado) {
+    $.getJSON(URL + "/estabelecimento/procura/", {
+        "campo": "nome",
+        "valor": termoPesquisado
+    })
+            .success(renderizarLista)
+            .then(ativarCollapsible)
+            .then(vincularEventos)
+            ;
+};
+var buscarIdEstabelecimento = function (idEstabelecimento) {
+    $.getJSON(URL + "/estabelecimento/" + idEstabelecimento)
+            .success(renderizarLista)
+            .then(ativarCollapsible)
+            .then(vincularEventos)
+            ;
+};
+var buscarTodosEstabelecimentos = function () {
+    $.getJSON(URL + "/estabelecimento/all")
+            .success(renderizarLista)
+            .then(ativarCollapsible)
+            .then(vincularEventos)
+            ;
+};
+var obterTermoPesquisado = function (e) {
+    var termo = $("#txt-pesquisa").val();
+    if (termo === "") {
+        buscarTodosEstabelecimentos();
+    } else if (isNaN(termo)) {
+        buscarNomeEstabelecimento(termo);
+    } else {
+        buscarIdEstabelecimento(termo);
+    }
 
-    var btnPesquisar = $("#btn-pesquisar");
+};
 
-    var buscarNomeEstabelecimento = function (termoPesquisado) {
-        $.getJSON(URL + "/estabelecimento/procura/", {
-            "campo": "nome",
-            "valor": termoPesquisado
-        })
-                .success(renderizarLista)
-                .then(ativarCollapsible)
-                .then(vincularEventos)
-                ;
+var vincularEventos = function () {
+    var btnMapa = $(".btn-mapa");
+    btnMapa.on("click", obterInformacoesEstabelecimento);
+    btnPesquisar.on("click", obterTermoPesquisado);
+};
+
+var initialize = function () {
+
+    directionsService = new google.maps.DirectionsService();
+    geocoder = new google.maps.Geocoder();
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: centroMapa
+    });
+    var rendererOptions = {
+        map: map,
+        suppressMarkers: true
     };
 
-    var buscarIdEstabelecimento = function (idEstabelecimento) {
-        $.getJSON(URL + "/estabelecimento/" + idEstabelecimento)
-                .success(renderizarLista)
-                .then(ativarCollapsible)
-                .then(vincularEventos)
-                ;
-    };
+};
 
-    var buscarTodosEstabelecimentos = function () {
-        $.getJSON(URL + "/estabelecimento/all")
-                .success(renderizarLista)
-                .then(ativarCollapsible)
-                .then(vincularEventos)
-                ;
-    };
+var inicializarPagina = function () {
+    //buscarEstabelecimentos();
+    initialize();
+    vincularEventos();
+};
 
-    var obterTermoPesquisado = function (e) {
-        var termo = $("#txt-pesquisa").val();
-        if (termo === "") {
-            buscarTodosEstabelecimentos();
-        } else if (isNaN(termo)) {
-            buscarNomeEstabelecimento(termo);
-        } else {
-            buscarIdEstabelecimento(termo);
-        }
-
-    };
-
-    var vincularEventosClick = function () {
-        btnPesquisar.on("click", obterTermoPesquisado);
-    };
-
-    inicializarPagina();
-});
+inicializarPagina();
